62c913a71a52a85238644122d1d891adaa427ca2
Jhossed Pineda Molina <jhossedmolina@gmail.com>
2023-04-20 23:39:23 -0500

    Installing Fluent Validation

--- InventoryManagementSystem.API/Program.cs
+++ InventoryManagementSystem.API/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using InventoryManagementSystem.Infraestructure.Migrations;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 
 var app = builder.Build();
 
